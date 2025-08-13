8f557c953afcce6c58e509568f37171ff480e119
lavishatrey <lavishatrey@gmail.com>
2025-08-13 19:41:44 +0530

    bug fixed

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '10mb' }))
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://hostel-management-app-three.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
